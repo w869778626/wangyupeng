89e0f993e740f137a48d0decdf6561659a0b6776
wangyp <869778626@qq.com>
2024-04-23 12:10:10 +0800

    todoList

--- vue2-object/src/components/HelloWorld.vue
+++ vue2-object/src/components/HelloWorld.vue
@@ -1,58 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
+    <input v-model="newItem" @keyup.enter="addItem" placeholder="Add item" />
+    <button @click="addItem">新建</button>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(item, index) in items" :key="index+item.text">
+        <div>第 {{ change(index + 1) }}条</div>
+    
+        <input v-show="item.isEdit" v-model="item.text" @keyup.enter="addItem" placeholder="Add item" />
+        <span v-show="!item.isEdit"> {{ item.text }}</span>
+        <div>
+          <button @click="removeItem(index)">删除</button>
+          <button @click="edit(item,index)">{{ item.isEdit?'保持' : '修改' }}</button>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      newItem: "",
+      items: [],
+      editContainer:''
+    };
+  },
+  methods: {
+    change(value) {
+      const chineseNumbers = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九"];
+      const chineseUnits = ["", "十", "百", "千", "万", "亿"];
+
+      if (value === 0) {
+        return chineseNumbers[0];
+      }
+
+      let result = "";
+      let unitIndex = 0;
+
+      const valueOle = value;
+
+      while (value > 0) {
+        const digit = value % 10;
+        if (digit !== 0 || unitIndex === 1) {
+          result = chineseNumbers[digit] + chineseUnits[unitIndex] + result;
+        }
+        value = Math.floor(value / 10);
+        unitIndex++;
+      }
+
+      if (valueOle > 9 && valueOle < 20) {
+        // 把10：十一，处理为：十
+        result = result.slice(1);
+        console.log("111", result.slice(1));
+      }
+
+      return result;
+    },
+    addItem() {
+      if (this.newItem.trim()) {
+        this.items.push({ text: this.newItem.trim(), isEdit:false});
+        this.newItem = "";
+      }
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    },
+    edit(item,index){
+      if(item.isEdit){
+        item.isEdit=false;
+        this.items[index]=item
+      }else{
+        item.isEdit=true;
+      this.items[index]=item
+      }
+    }
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+/* 样式内容按需添加 */
 </style>
